34bba9e3df9aa985eb31c1bcf10fab7e6bfc290b
Isaiah Jan Navasquez <isaiahnavasquez@gmail.com>
2018-05-30 11:49:49 +0800

    profect configuration models created

---
+++ catstagram/cats/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200)
+    following = models.ManyToManyField('self', related_name='followers', blank=True)
+    # profile_picture imagefield
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    caption = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    likers = models.ManyToManyField(User, blank=True)
+    # picture imagefield
+
+    def __str__(self):
+        if len(self.caption) > 15:
+            return self.caption[15]
+        else:
+            return self.caption
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts')
+    content = models.CharField(max_length=200)
+
+    def __str__(self):
+        if len(self.content) > 15:
+            content = self.content[15]
+        else:
+            content = self.content
+
+        return '@{} to @{} : {}...'.format(self.author.username,
+                                        self.post.author.username,
+                                        content)
